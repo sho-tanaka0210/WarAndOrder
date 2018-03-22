c7b567ae2024ec2d6d4a0fff6c1595acaa73bfd7
Sho Tanaka <canaryyellow.nl@gmail.com>
2018-03-23 00:48:48 +0900

    First commit.

---
+++ Mainbot.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+require('date-utils');
+var now = new Date();
+
+var channelId = '';
+
+client.on("ready", () => {
+	console.log("ログインしました。");
+});
+
+client.on("message", (message) => {
+	
+	if (message.content.startsWith("!領地防衛")) {
+		if(now.toFormat('DDD') === "Sun"){
+			message.guild.channels.get(channelId).send("**領地防衛は本日22時半からです!**");
+		}
+		else{
+			message.guild.channels.get(channelId).send("領地防衛は本日ではありません!");
+		}
+	}
+	if (message.content.startsWith("!連盟挑戦")) {
+		if(now.toFormat('DDD') === "Fri"){
+			message.guild.channels.get(channelId).send("**連盟挑戦は本日22時半からです!**");
+		}else{
+			message.guild.channels.get(channelId).send("連盟挑戦は本日ではありません!");
+		}
+	}
+	
+	/*message.channel.startTyping();
+	message.channel.stopTyping();*/
+});
+
+client.login("NDEzMzUyNDMyODMyMDg2MDU2.DWXm_A.JYDiw006SkcoTSpPPZaElqi_Woc");
